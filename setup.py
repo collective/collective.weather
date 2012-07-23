7b8b51272cde55aea24cfb36e37c2a7a040b0bd2
hvelarde <hector.velarde@gmail.com>
2012-07-23 14:36:45 -0300

    fixed package distribution and development buildout configurations

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup, find_packages
 import os
 
 version = '1.0a1.dev0'
-
 description = "This product gives you weather support to Plone"
 long_description = open("README.txt").read() + "\n" + \
                    open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
@@ -11,10 +10,8 @@ long_description = open("README.txt").read() + "\n" + \
 
 setup(name='collective.weather',
       version=version,
-      description="",
+      description=description,
       long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Web Environment",
@@ -30,28 +27,27 @@ setup(name='collective.weather',
         "Programming Language :: Python :: 2.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='weather plone',
+      keywords='api google noaa plone weather yahoo',
       author='Franco Pellegrini',
       author_email='frapell@ravvit.net',
       url='http://svn.plone.org/svn/collective/',
       license='gpl',
       packages=find_packages('src'),
-      package_dir = {'': 'src'},
+      package_dir={'': 'src'},
       namespace_packages=['collective', ],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
           'setuptools',
-          # -*- Extra requirements: -*-
+          'pywapi',
+          'Plone>=4.1',
           'plone.app.registry',
           'collective.z3cform.widgets',
-          'pywapi==0.2.2'
       ],
       extras_require={
         'test': ['plone.app.testing']
         },
       entry_points="""
-      # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
       target = plone
       """,
