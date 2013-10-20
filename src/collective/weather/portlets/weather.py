ef88534b04daa36890b0b6836ceadbe8ab6c3e27
Marcos F. Romero <marcosfromero@gmail.com>
2013-10-20 10:35:19 -0300

    Removed time_to_live setting from portlet

--- src/collective/weather/portlets/weather.py
+++ src/collective/weather/portlets/weather.py
@@ -3,11 +3,10 @@
 from collective.weather import _
 from collective.weather.interfaces import IWeatherUtility
 from plone.app.portlets.portlets import base
+from plone.app.portlets.browser.formhelper import NullAddForm
 from plone.portlets.interfaces import IPortletDataProvider
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from zope import schema
 from zope.component import getUtility
-from zope.formlib import form
 from zope.interface import implements
 
 
@@ -15,20 +14,12 @@ class IWeatherPortlet(IPortletDataProvider):
     """ A weather portlet
     """
 
-    time_to_live = schema.Float(
-        title=_(u"Time to live"),
-        description=_("After the specified minutes, the portlet will refresh its data"),
-        required=True)
-
 
 class Assignment(base.Assignment):
     """ Portlet assignment.
     """
 
     implements(IWeatherPortlet)
-
-    def __init__(self, time_to_live=30):
-        self.time_to_live = time_to_live
 
     @property
     def title(self):
@@ -49,18 +40,9 @@ class Renderer(base.Renderer):
         self.weather_info = weather_utility.get_weather_info()
 
 
-class AddForm(base.AddForm):
+class AddForm(NullAddForm):
     """Portlet add form.
     """
 
-    form_fields = form.Fields(IWeatherPortlet)
-
-    def create(self, data):
-        return Assignment(**data)
-
-
-class EditForm(base.EditForm):
-    """Portlet edit form.
-    """
-
-    form_fields = form.Fields(IWeatherPortlet)
+    def create(self):
+        return Assignment()
