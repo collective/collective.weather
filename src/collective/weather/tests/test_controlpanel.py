4fb4b61547982483c56097685ea8390ca0fa28f0
Marcos F. Romero <marcosfromero@gmail.com>
2013-10-20 23:26:59 -0300

    Removed Google Weather services

--- src/collective/weather/tests/test_controlpanel.py
+++ src/collective/weather/tests/test_controlpanel.py
@@ -11,7 +11,6 @@ from plone.app.testing import setRoles
 from plone.registry.interfaces import IRegistry
 
 from collective.weather.config import PROJECTNAME
-from collective.weather.browser.interfaces import IGoogleWeatherSchema
 from collective.weather.browser.interfaces import INoaaWeatherSchema
 from collective.weather.browser.interfaces import IYahooWeatherSchema
 from collective.weather.testing import INTEGRATION_TESTING
@@ -52,49 +51,6 @@ class ControlPanelTestCase(unittest.TestCase):
                    for a in self.controlpanel.listActions()]
         self.assertTrue('WeatherSettings' not in actions,
                         'control panel was not removed')
-
-
-class GoogleRegistryTestCase(unittest.TestCase):
-
-    layer = INTEGRATION_TESTING
-
-    def setUp(self):
-        self.portal = self.layer['portal']
-        self.registry = getUtility(IRegistry)
-        self.settings = self.registry.forInterface(IGoogleWeatherSchema)
-        setRoles(self.portal, TEST_USER_ID, ['Manager'])
-
-    def test_use_google_record_in_registry(self):
-        self.assertTrue(hasattr(self.settings, 'use_google'))
-        self.assertEqual(self.settings.use_google, False)
-
-    def test_google_location_ids_record_in_registry(self):
-        self.assertTrue(hasattr(self.settings, 'google_location_ids'))
-        self.assertEqual(self.settings.google_location_ids, [])
-
-    def test_google_language_record_in_registry(self):
-        self.assertTrue(hasattr(self.settings, 'google_language'))
-        self.assertEqual(self.settings.google_language, None)
-
-    def test_google_units_record_in_registry(self):
-        self.assertTrue(hasattr(self.settings, 'google_units'))
-        self.assertEqual(self.settings.google_units, u'metric')
-
-    def get_record(self, record):
-        """ Helper function; it raises KeyError if the record is not in the
-        registry.
-        """
-        prefix = 'collective.weather.browser.interfaces.IGoogleWeatherSchema.'
-        return self.registry[prefix + record]
-
-    def test_records_removed_on_uninstall(self):
-        # XXX: I haven't found a better way to test this; anyone?
-        qi = self.portal['portal_quickinstaller']
-        qi.uninstallProducts(products=[PROJECTNAME])
-        self.assertRaises(KeyError, self.get_record, 'use_google')
-        self.assertRaises(KeyError, self.get_record, 'google_location_ids')
-        self.assertRaises(KeyError, self.get_record, 'google_language')
-        self.assertRaises(KeyError, self.get_record, 'google_units')
 
 
 class YahooRegistryTestCase(unittest.TestCase):
